9a589c395434e0fdd6ee513ebd6d3d0dc5a1c9b0
Hadrien <noemail>
2018-05-15 06:40:05 -0700

    added script to build configuration

---
+++ build_config.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+
+function setup_env {
+
+set +h
+umask 022
+LFS=/home/hadri/cxs/baseware
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+sudo chroot "$LFS" /tools/bin/env -i HOME=/root TERM="$TERM" PS1='\u:\w\$ ' PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin CXS_BUILDDD=1 /config.sh
+
+}
+
+function config_network {
+
+cat > /etc/resolv.conf << "EOF"
+# Begin /etc/resolv.conf
+
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+
+# End /etc/resolv.conf
+EOF
+
+}
+
+function install_bootscripts {
+
+cd /sources/
+tar xvf lfs-bootscripts-20120229.tar.bz2
+cd lfs-bootscripts-20120229
+
+make install
+
+cd /sources/
+rm -rf lfs-bootscripts-20120229
+
+}
+
+function configure_sysvinit {
+
+cat > /etc/inittab << "EOF"
+# Begin /etc/inittab
+
+id:3:initdefault:
+
+si::sysinit:/etc/rc.d/init.d/rc S
+
+l0:0:wait:/etc/rc.d/init.d/rc 0
+l1:S1:wait:/etc/rc.d/init.d/rc 1
+l2:2:wait:/etc/rc.d/init.d/rc 2
+l3:3:wait:/etc/rc.d/init.d/rc 3
+l4:4:wait:/etc/rc.d/init.d/rc 4
+l5:5:wait:/etc/rc.d/init.d/rc 5
+l6:6:wait:/etc/rc.d/init.d/rc 6
+
+ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now
+
+su:S016:once:/sbin/sulogin
+
+1:2345:respawn:/sbin/agetty --noclear tty1 9600
+2:2345:respawn:/sbin/agetty tty2 9600
+3:2345:respawn:/sbin/agetty tty3 9600
+4:2345:respawn:/sbin/agetty tty4 9600
+5:2345:respawn:/sbin/agetty tty5 9600
+6:2345:respawn:/sbin/agetty tty6 9600
+
+# End /etc/inittab
+EOF
+
+}
+
+function configure_hostname {
+
+echo "HOSTNAME=cxs-system-lfs-7.1" > /etc/sysconfig/network
+
+}
+
+function configure_hwclock {
+
+cat > /etc/sysconfig/clock << "EOF"
+# Begin /etc/sysconfig/clock
+
+UTC=1
+
+# Set this to any options you might need to give to hwclock, 
+# such as machine hardware clock type for Alphas.
+CLOCKPARAMS=
+
+# End /etc/sysconfig/clock
+EOF
+
+}
+
+function configure_inputrc {
+
+cat > /etc/inputrc << "EOF"
+# Begin /etc/inputrc
+# Modified by Chris Lynn <roryo@roryo.dynup.net>
+
+# Allow the command prompt to wrap to the next line
+set horizontal-scroll-mode Off
+
+# Enable 8bit input
+set meta-flag On
+set input-meta On
+
+# Turns off 8th bit stripping
+set convert-meta Off
+
+# Keep the 8th bit for display
+set output-meta On
+
+# none, visible or audible
+set bell-style none
+
+# All of the following map the escape sequence of the value
+# contained in the 1st argument to the readline specific functions
+"\eOd": backward-word
+"\eOc": forward-word
+
+# for linux console
+"\e[1~": beginning-of-line
+"\e[4~": end-of-line
+"\e[5~": beginning-of-history
+"\e[6~": end-of-history
+"\e[3~": delete-char
+"\e[2~": quoted-insert
+
+# for xterm
+"\eOH": beginning-of-line
+"\eOF": end-of-line
+
+# for Konsole
+"\e[H": beginning-of-line
+"\e[F": end-of-line
+
+# End /etc/inputrc
+EOF
+
+}
+
+function configure_fstab {
+
+cat > /etc/fstab << "EOF"
+# Begin /etc/fstab
+
+# file system  mount-point  type     options             dump  fsck
+#                                                              order
+
+/dev/sdb1      /            ext4     defaults            1     1
+/dev/sdb2      swap         swap     pri=1               0     0
+proc           /proc        proc     nosuid,noexec,nodev 0     0
+sysfs          /sys         sysfs    nosuid,noexec,nodev 0     0
+devpts         /dev/pts     devpts   gid=4,mode=620      0     0
+tmpfs          /run         tmpfs    defaults            0     0
+devtmpfs       /dev         devtmpfs mode=0755,nosuid    0     0
+
+# End /etc/fstab
+EOF
+
+}
+
+function build_linux_kernel {
+
+cd /sources/
+tar xvf linux-3.2.6.tar.xz
+cd linux-3.2.6
+
+make mrproper
+make LC_ALL= menuconfig
+make
+make modules_install
+cp -v arch/x86/boot/bzImage /boot/vmlinuz-3.2.6-lfs-7.1
+cp -v System.map /boot/System.map-3.2.6
+cp -v .config /boot/config-3.2.6
+
+cd /sources/
+rm -rf linux-3.2.6
+
+}
+
+function configure_linux_kernel_modules_boot_order {
+
+install -v -m755 -d /etc/modprobe.d
+cat > /etc/modprobe.d/usb.conf << "EOF"
+# Begin /etc/modprobe.d/usb.conf
+
+install ohci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i ohci_hcd ; true
+install uhci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i uhci_hcd ; true
+
+# End /etc/modprobe.d/usb.conf
+EOF
+
+}
+
+if [ -z "$CXS_BUILDDD" ]; then
+   setup_env
+else
+   set +h
+   #config_network
+   #install_bootscripts
+   #configure_sysvinit
+   #configure_hostname
+   #configure_hwclock
+   #configure_inputrc
+   #configure_fstab
+   build_linux_kernel
+   #configure_linux_kernel_modules_boot_order
+   #/bin/bash
+fi
+
